4bfe76aca2e5f7a39c92b0059556025b084750fc
Mark David Billena <markdavidgebra@gmail.com>
2026-05-09 21:47:54 +0800

    Fixed production API

--- src/api.js
+++ src/api.js
@@ -1,6 +1,7 @@
 // src/api.js
 
-const BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:8000/api';
+// const BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:8000/api';
+const BASE_URL = 'https://bioglowsolutions.com/api';
 
 export async function fetchCatalog({ type = '', search = '', limit = 50, include = '' } = {}) {
   const params = new URLSearchParams();
